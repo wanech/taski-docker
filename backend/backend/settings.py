277c148b93e13b9f782f6f50f4702914b17891a0
ubuntu <ivan-yadrive@yandex.ru>
2026-06-23 21:16:36 +0000

    Add autostart

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -116,5 +116,5 @@ STATIC_ROOT = BASE_DIR / 'collected_static'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ORIGIN_WHITELIST = [
-    'http://localhost:3000'
+    'http://localhost:8000'
 ]
